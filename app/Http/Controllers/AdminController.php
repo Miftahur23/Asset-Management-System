0d1175994bf7041af51398ddd1e0087ca655660d
Miftahur23 <miftahur109970@gmail.com>
2021-12-23 13:24:35 +0600

    edit partial done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -50,7 +50,7 @@ class AdminController extends Controller
 
     public function EditedBranch()
     {
-        
+
         return redirect()->route('show.branch')->with('edited','Branch Edited Successfully');
     }
 
