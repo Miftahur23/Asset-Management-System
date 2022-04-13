90be69ad506468068258f296e7e43e235abb8db5
Miftahur23 <miftahur109970@example.com>
2022-04-13 17:47:07 +0000

    event

---
+++ app/Events/DepartmentEvent.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DepartmentEvent
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public $deletedDepartment;
+    
+    public function __construct($department)
+    {
+        $this->deletedDepartment=$department;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
---
+++ app/Listeners/DepartmentDelete.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\DepartmentEvent;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class DepartmentDelete
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\DepartmentEvent  $event
+     * @return void
+     */
+    public function handle(DepartmentEvent $event)
+    {
+        if(Cache::has('Departments'))
+        {
+            Cache::forget('Departments');
+        }
+    }
+}
